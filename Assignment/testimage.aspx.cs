186150573fd15c4006ad8ab52e075de75a67f495
Junlim2804 <limtj-pa16@student.tarc.edu.my>
2018-12-14 16:49:16 +0800

    14/12 4.49pm

---
+++ Assignment/testimage.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment
+{
+    public partial class testimage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string conn = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            SqlConnection connection = new SqlConnection(conn);
+            connection.Open();
+            string sql = "SELECT image FROM test WHERE id='21' ";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            object img = cmd.ExecuteScalar();
+            byte[] arr = (byte[])img;
+            string base64String = Convert.ToBase64String(arr, 0, arr.Length);
+            Image1.ImageUrl = "data:image/jpg;base64," + base64String;
+            connection.Close);
+        }
+    }
+}
---
+++ insertimage.sql
@@ -0,0 +1,3 @@
+﻿insert into test(id,[image]) 
+SELECT '21',BulkColumn 
+FROM Openrowset( Bulk 'C:\Users\magic\Desktop\2.png', Single_Blob) as img
